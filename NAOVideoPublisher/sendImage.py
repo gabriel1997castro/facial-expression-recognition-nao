92b76ec012159be851184e3cb2e0c5e7907e79be
Eric do Vale de Castro <eric.castro@coreconsulting.com.br>
2022-07-16 11:51:53 -0300

    feat: Enviando e recebendo por pacotes os frames do streming de video

--- NAOVideoPublisher/sendImage.py
+++ NAOVideoPublisher/sendImage.py
@@ -1,13 +1,15 @@
-import socket, cv2, base64, imutils
+import socket, cv2, base64, imutils, math
+import pickle
 
 host = '127.0.0.1'
 portHost = 8081
 
-vid = cv2.VideoCapture('video1.mkv')
-vid.set(cv2.CAP_PROP_FRAME_WIDTH, 840)
-vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 620)
+vid = cv2.VideoCapture(0) # Pela webcam
+# vid = cv2.VideoCapture('video1.mkv') # Testes para transferencia de video
+vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
-BUFF_SIZE = 65536
+BUFF_SIZE = 65000
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
@@ -18,13 +20,38 @@ while True:
     print
     msg, addr = server_socket.recvfrom(BUFF_SIZE)
     print('GOT connection from ', addr)
-    teste = 400
     while(vid.isOpened()):
         ret, frame = vid.read()
-        frame = imutils.resize(frame, width=teste)
+
+        HORIZONTAL_FLIP_INDEX = 1
+
+        frame = cv2.flip(frame, HORIZONTAL_FLIP_INDEX) # FLIPPER AS IMAGENS
         encode, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 80])
-        message = base64.b64encode(buffer)
-        server_socket.sendto(message, addr)
+
+        if encode:
+            buffer = buffer.tobytes()
+            buffer_size = len(buffer)
+        
+        num_packs = 1
+        if buffer_size > BUFF_SIZE:
+            num_packs = math.ceil(float(buffer_size)/BUFF_SIZE)
+        
+        frame_info = {"packs":int(num_packs)}
+
+        server_socket.sendto(pickle.dumps(frame_info), addr)
+
+        left = 0
+        right = BUFF_SIZE
+
+        for index in range(int(num_packs)):
+            data = buffer[left:right]
+            left = right
+            right += BUFF_SIZE
+
+            server_socket.sendto(data, addr)
+
+        # message = base64.b64encode(buffer)
+        # server_socket.sendto(message, addr)
         cv2.imshow('frame', frame)
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
--- NAOVideoSubscriber/clientImage.py
+++ NAOVideoSubscriber/clientImage.py
@@ -1,8 +1,10 @@
+from email.headerregistry import Address
 import cv2, socket
 import numpy as np
 import base64
+import pickle
 
-BUFF_SIZE = 65536
+BUFF_SIZE = 65000
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
 HOST = '127.0.0.1'
@@ -13,11 +15,27 @@ client_socket.sendto(message.encode('utf-8'), (HOST, portHost))
 
 while True:
     packet,_ = client_socket.recvfrom(BUFF_SIZE)
-    data = base64.b64decode(packet, ' /')
-    npdata = np.fromstring(data, dtype=np.uint8)
-    frame = cv2.imdecode(npdata, 1)
-    cv2.imshow('Recebendo a imagem', frame)
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        client_socket.close()
-        break
+
+    if len(packet) < 100:
+        frame_info = pickle.loads(packet)
+
+        if frame_info:
+            nums_of_packs =  frame_info["packs"]
+
+            for index in range(nums_of_packs):
+                data, address = client_socket.recvfrom(BUFF_SIZE)
+
+                if index == 0:
+                    buffer = data
+                else:
+                    buffer += data
+            frame = np.frombuffer(buffer, dtype=np.uint8)
+            frame = frame.reshape(frame.shape[0], 1)
+            # data = base64.b64decode(packet, ' /')
+            # npdata = np.fromstring(data, dtype=np.uint8)
+            frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+            cv2.imshow('Recebendo a imagem', frame)
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                client_socket.close()
+                break
