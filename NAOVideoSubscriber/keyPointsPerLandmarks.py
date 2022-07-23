e59b6c3d12bc75fa3d3b468e417421391661d9cd
Eric do Vale de Castro <eric.castro@coreconsulting.com.br>
2022-07-23 18:23:17 -0300

    feat: Inicia Mapeamento dos pontos do rosto

--- NAOVideoSubscriber/keyPointsPerLandmarks.py
+++ NAOVideoSubscriber/keyPointsPerLandmarks.py
@@ -16,27 +16,103 @@ message = 'AZUL'
 client_socket.sendto(message.encode('utf-8'), (HOST, portHost))
 
 lipsOutter = {
-61: True,
-185: True,
-40: True,
-39: True,
-37: True,
-0: True,
-267: True,
-269: True,
-270: True,
-409: True,
-291: True,
-375: True,
-321: True,
-405: True,
-314: True,
-17: True,
-84: True,
-181: True,
-91: True,
-146: True,
-61: True
+# 61: True,
+# 185: True,
+# 40: True,
+# 39: True,
+# 37: True,
+# 0: True,
+# 267: True,
+# 269: True,
+# 270: True,
+# 409: True,
+# 291: True,
+# 375: True,
+# 321: True,
+# 405: True,
+# 314: True,
+# 17: True,
+# 84: True,
+# 181: True,
+# 91: True,
+# 146: True,
+# 61: True,
+
+# Silhueta do rosto 
+10: True,
+109: True,
+67: True,
+103: True,
+54: True,
+21: True,
+162: True,
+127: True,
+234: True,
+93: True,
+132: True,
+58: True,
+172: True,
+136: True,
+150: True,
+149: True,
+176: True,
+148: True,
+152: True,
+377: True,
+400: True,
+378: True,
+379: True,
+365: True,
+397: True,
+288: True,
+361: True,
+323: True,
+454: True,
+356: True,
+389: True,
+251: True,
+284: True,
+332: True,
+297: True,
+338: True,
+
+# Sombrancelhas
+8: True,
+9: True,
+46: True,
+52: True,
+107: True,
+336: True,
+282: True,
+276: True,
+
+# Olho esquerdo
+33: True,
+133: True,
+144: True,
+160: True,
+158: True,
+153: True,
+157: True,
+
+#Bochecha Esquerda
+50: True,
+101: True,
+207: True,
+
+# Olho direito
+263: True,
+362: True,
+385: True,
+380: True,
+384: True,
+387: True,
+373: True,
+
+#Bochecha direito
+280: True,
+330: True,
+427: True,
 }
 
 class FaceMeshDetector:
@@ -81,6 +157,9 @@ class FaceMeshDetector:
                 # print('AZUL', id)
                 ih, iw, ic = img.shape
                 x, y = int(lm.x*iw), int(lm.y*ih)
+                # if id % 2 == 0:
+                # cv2.putText(img, str(id), (x,y), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
+                # cv2.putText(empty, str(id), (x,y), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
                 if lipsOutter.get(id):
                     # cv2.putText(img, str(id), (x,y), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
                     img = cv2.circle(img, (x,y), 1, (0, 255, 0), -1)
